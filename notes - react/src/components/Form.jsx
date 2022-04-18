8595064a38baf78e6223c56b6a308ae40c7ef9f9
MelinaGimenez <gimenez.melina@hotmail.com>
2022-04-17 23:04:31 -0300

    validación de formulario

--- notes - react/src/components/Form.jsx
+++ notes - react/src/components/Form.jsx
@@ -1,6 +1,36 @@
+import { useState, useEffect } from 'react';
+
 function Form() {
 
+    //Valores originales de los campos
+
+    const [pet, setPet] = useState('');
+    const [owner, setOwner] = useState('');
+    const [email, setEmail] = useState('');
+    const [date, setDate] = useState('');
+    const [symptoms, setSymptoms] = useState('');
+
+    //Errores
+
+    const [error, setError] = useState(false)
+
+    //Validación del formulario
+
+    const handleSubmit = (e) => {
+        
+        e.preventDefault();
+
+        if( [pet, owner, email, date, symptoms].includes('')){
+            console.log('Atención: Campos Vacíos')
+            setError(true)
+        }else{
+            setError(false)
+        }
+
+    } 
+
     return(
+
         <div className="md:w-2/3 lg:w-2/5 text-center m-3 ">
             <h2 className="font-black text-3xl text-center mt-10">
                 Seguimiento Pacientes
@@ -11,7 +41,15 @@ function Form() {
                     Administralos
                 </span>
             </p>
-            <form className="bg-white shadow-md rounded-lg py-10 px-5 border-dashed border-2 border-indigo-300">
+
+            <form 
+                onSubmit={handleSubmit}
+                className="bg-white shadow-md rounded-lg py-10 px-5 border-dashed border-2 border-indigo-300">
+                { error && 
+                (<div className='bg-red-800 text-white text-center p-3 font-bold mb-10 rounded-md'>
+                    <p>Debes llenar todos los campos</p>
+                </div>)
+                }
                 <div className="mb-5">
                     <label htmlFor="pet" className="block text-gray-700 uppercase font-bold">
                         Nombre Mascota
@@ -21,6 +59,9 @@ function Form() {
                         type="text"
                         placeholder="Nombre de la Mascota"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        //Guardar lo escrito en formularios
+                        value= {pet}
+                        onChange= {(e) => setPet(e.target.value)}
                     />
                 </div>
                 <div className="mb-5">
@@ -32,6 +73,8 @@ function Form() {
                         type="text"
                         placeholder="Nombre del Propietario"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value= {owner}
+                        onChange= {(e) => setOwner(e.target.value)}
                     />
                 </div>
                 <div className="mb-5">
@@ -43,16 +86,20 @@ function Form() {
                         type="text"
                         placeholder="Email"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value= {email}
+                        onChange= {(e) => setEmail(e.target.value)}
                     />
                 </div>
                 <div className="mb-5">
-                    <label htmlFor="alta" className="block text-gray-700 uppercase font-bold">
+                    <label htmlFor="date" className="block text-gray-700 uppercase font-bold">
                         Alta
                     </label>
                     <input
-                        id="alta"
+                        id="date"
                         type="date"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value= {date}
+                        onChange= {(e) => setDate(e.target.value)}
                     />
                 </div>
                 <div className="mb-5">
@@ -63,6 +110,8 @@ function Form() {
                         id="symptoms"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                         placeholder="Describe los síntomas"
+                        value= {symptoms}
+                        onChange= {(e) => setSymptoms(e.target.value)}
                     />
                 </div>
                 <input 
